ac0b71d199c66d9245f796a245c91bb62ec381cc
Joa Ebert <joaebert@googlemail.com>
2012-04-08 13:27:50 +0200

    Renamed HttpResponseInput to HttpRequestInput.

--- hector-web/src/main/scala/hector/http/HttpRequest.scala
+++ hector-web/src/main/scala/hector/http/HttpRequest.scala
@@ -10,4 +10,31 @@ trait HttpRequest extends Serializable {
   def headers: Seq[HttpHeader]
 
   def authType: Option[HttpAuthType]
+
+  def info: HttpRequestInfo = new HttpRequestInfo(this)
+}
+
+@inline
+final class HttpRequestInfo(request: HttpRequest) {
+  /** Whether or not the client would not like to be tracked. */
+  def doNotTrack: Boolean =
+    request.headers exists { 
+      case XDoNotTrack(_) | DNT(_) => true
+      case _ => false
+    }
+  
+  /** Whether or not the request was made using Android. */
+  def isAndroid: Boolean = false
+
+  /** Whether or not the request was made using iOS. */
+  def isIOS: Boolean = false
+
+  /** Whether or not the request was made using Windows. */
+  def isWindows: Boolean = false
+
+  /** Whether or not the request was made using Linux. */
+  def isLinux: Boolean = false
+
+  /** Whether or not the request was made using OS X.*/
+  def isOSX: Boolean = false
 }
--- hector-web/src/main/scala/hector/http/HttpRequestInput.scala
+++ hector-web/src/main/scala/hector/http/HttpRequestInput.scala
@@ -2,6 +2,6 @@ package hector.http
 
 /**
  */
-trait HttpResponseInput {
+trait HttpRequestInput {
   //TODO(joa): this is going to be tricky. need to have an actor with the request that sits on the front-end node and this should be a ref to that actor.
 }
